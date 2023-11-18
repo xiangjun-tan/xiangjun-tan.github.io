5668072cfee25fa56f54af7190390658816b5df7
xiangjun-tan <61309784+xiangjun-tan@users.noreply.github.com>
2023-11-19 02:47:38 +1100

    Update scale.fix.js

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -8,10 +8,10 @@
             }
         },
         initialize = function() {
-            changeViewportContent("width=device-width, minimum-scale=2.0, maximum-scale=2.2");
+            changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
         },
         gestureStart = function() {
-            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=2.2");
+            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.8");
         },
         gestureEnd = function() {
             initialize();
